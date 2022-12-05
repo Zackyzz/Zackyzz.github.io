0ca9bea8782002b51f509f6e2696088deb31c486
burghelea zaharia <legendaryproyi@yahoo.com>
2022-12-05 10:18:10 +0200

    Added new menu

---
+++ main.js
@@ -0,0 +1,17 @@
+const path = document.querySelectorAll('nav ul ul li a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 100) {
+            current = section.getAttribute('id');
+        }
+    });
+    
+    path.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav ul ul li a[href*= ' + current + ']').classList.add('active');
+    });
+});
