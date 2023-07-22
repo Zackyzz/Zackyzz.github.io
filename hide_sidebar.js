d17fe03d8b140842dc2ef02adb57ed55705fefa6
burghelea zaharia <legendaryproyi@yahoo.com>
2023-07-22 04:03:21 +0300

    Updated navbar

--- hide_sidebar.js
+++ hide_sidebar.js
@@ -1,18 +1,12 @@
 const sidebar = document.querySelector('#sidebar');
 const handle = document.querySelector('#handle');
 
-let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-sidebar.classList.toggle('collapsed', isMobile);
-handle.classList.toggle('handle-collapsed', isMobile);
-
 const toggleSidebar = () => {
-    isMobile = !isMobile;
-    sidebar.classList.toggle('collapsed', isMobile);
-    handle.classList.toggle('handle-collapsed', isMobile);
+    sidebar.classList.toggle('collapsed');
+    handle.classList.toggle('handle-collapsed');
 };
 
 handle.addEventListener('click', function() {
-    sidebar.classList.toggle('collapsed');
+    toggleSidebar();
     document.querySelector('.page-content').style.paddingLeft = sidebar.classList.contains('collapsed') ? '10%' : '27.5%';
 });
