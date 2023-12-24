1c297d5ad12d31dd2e0bc767fee562b948331327
burghelea zaharia <legendaryproyi@yahoo.com>
2023-12-24 15:21:04 +0200

    x6

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,8 +1,12 @@
+const darkModeToggle = document.querySelector('#darkModeToggle');
 const darkMode = localStorage.getItem('darkMode') === 'true';
 
-if (darkMode === 'false') {
+if (darkMode === false) {
     document.body.classList.add('light-mode');
     darkModeToggle.checked = true;
+} else {
+    document.body.classList.remove('light-mode');
+    darkModeToggle.checked = false;
 }
 
 darkModeToggle.addEventListener('change', () => {
